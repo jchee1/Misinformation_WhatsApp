f46e1653a7de982fbe6f611e25a7560bfa04b636
Jason Chee <jchee1@uchicago.edu>
2020-07-07 17:59:05 -0400

    added num of msgs per user per day + num of text, images, and url msgs sent from user + the total

--- parser.py
+++ parser.py
@@ -1,8 +1,13 @@
 import re
 
-with open('/Users/Jason/Downloads/_chat.txt', 'r') as fp:
+with open('/Users/Jason/Downloads/chat.txt', 'r') as fp:
     line = fp.readline()
     msgs = []
+
+    #variables to count total num of urls, images, and text chats
+    num_urls = 0
+    num_img = 0
+    num_txt = 0
 
     #create dictionary for contacts
     contacts = dict()
@@ -10,8 +15,12 @@ with open('/Users/Jason/Downloads/_chat.txt', 'r') as fp:
     #create dictionary for dates
     Dates = dict()
 
-    #list for anonymous user names
-    user_lst = []
+    #created nested dictionary to get num of msgs per user per day
+    d = {}
+
+    source = {'URL': { },
+            'IMG' : { },
+            'TXT' : { }}
 
     #have variable store total number of messages in chat
     total_num = 0
@@ -62,14 +71,50 @@ with open('/Users/Jason/Downloads/_chat.txt', 'r') as fp:
                 user_id = list(contacts.keys()).index(sender)
                 sender = "User" + str(user_id+1)
 
+            #num of msgs per user per day
+            if date in d:
+                if sender in d[date]:
+                    d[date][sender] += 1
+                else:
+                    d[date][sender] = 1
+            else:
+                d[date] = {}
+                if sender in d[date]:
+                    d[date][sender] += 1
+                else:
+                    d[date][sender] = 1 
+
             msg = msgsplit[1].rstrip("\n")
 
             #get url with findall fct
             urls = re.findall(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', msg)
             #msgs.append({"date":date, "time":time, "sender":sender, "message":msg})
 
+            #get img file from line if available
+            imgsplit = re.split("<attached: |>", msg)
+                
+
             if urls and int(year) == 20:
                 msgs.append({"date":date, "time":time, "sender":sender, "URL":urls})
+            if urls:
+                num_urls += 1
+                if sender in source['URL']:
+                    source['URL'][sender] += 1
+                else:
+                    source['URL'][sender] = 1
+
+            if len(imgsplit[0]) == 0:
+                num_img += 1
+                if sender in source['IMG']:
+                    source['IMG'][sender] += 1
+                else:
+                    source['IMG'][sender] = 1
+            if not urls and len(imgsplit[0]) != 0:
+                num_txt += 1
+                if sender in source['TXT']:
+                    source['TXT'][sender] += 1
+                else:
+                    source['TXT'][sender] = 1
         
             
         else:
@@ -102,15 +147,51 @@ with open('/Users/Jason/Downloads/_chat.txt', 'r') as fp:
                 user_id = list(contacts.keys()).index(sender)
                 sender = "User" + str(user_id+1)
 
+            if date in d:
+                if sender in d[date]:
+                    d[date][sender] += 1
+                else:
+                    d[date][sender] = 1
+            else:
+                d[date] = {}
+                if sender in d[date]:
+                    d[date][sender] += 1
+                else:
+                    d[date][sender] = 1 
+
             urls = re.findall(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', msg)
             #msgs.append({"date":date, "time":time, "sender":sender, "message":msg})
+            
+            imgsplit = re.split("<attached: |>", msg)
+
+
             if urls and int(year) == 20:
                 msgs.append({"date":date, "time":time, "sender":sender, "URL":urls})
+            if urls:
+                num_urls += 1
+                if sender in source['URL']:
+                    source['URL'][sender] += 1
+                else:
+                    source['URL'][sender] = 1
+
+            if len(imgsplit[0]) == 0:
+                num_img += 1
+                if sender in source['IMG']:
+                    source['IMG'][sender] += 1
+                else:
+                    source['IMG'][sender] = 1
+            if not urls and len(imgsplit[0]) != 0:
+                num_txt += 1
+                if sender in source['TXT']:
+                    source['TXT'][sender] += 1
+                else:
+                    source['TXT'][sender] = 1
             
 
         total_num += 1
         line = fp.readline()
 
+    print(imgsplit)
     end_date = date
     print("Number of total messages:", total_num)
     print("Number of messages in 2020:", num_2020)
@@ -126,4 +207,22 @@ with open('/Users/Jason/Downloads/_chat.txt', 'r') as fp:
     for key in list(Dates.keys()):
         print("Number of messages on",key, ":", Dates[key])
 
+    #num of msg per user per day
+    for dat, user in d.items():
+        print("Date:", dat)
+
+        for key in user:
+            print(key + ":", user[key])
+    
+        
+
     print(msgs)
+    print("URLS:", num_urls)
+    print("IMGS:", num_img)
+    print("TXT:", num_txt)
+
+    #print each source message sent from each user
+    for sources, participant in source.items():
+        print(sources + ":")
+        for key in participant:
+            print(key + ":", participant[key])
