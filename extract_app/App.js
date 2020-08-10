8c841fdf6891417bc10d2fcd7b3ec0bb83a3bdd3
Jason Chee <jchee1@uchicago.edu>
2020-08-10 14:31:16 -0400

    temp fix

--- extract_app/App.js
+++ extract_app/App.js
@@ -10,7 +10,7 @@
  import {StyleSheet, Text, View, Image} from 'react-native';
  import ShareMenu from 'react-native-share-menu';
  import { zip, unzip, unzipAssets, subscribe } from 'react-native-zip-archive'
- import { MainBundlePath, DocumentDirectoryPath, TemporaryDirectoryPath, readFile, readDir, stat, copyFile } from 'react-native-fs'
+ import { MainBundlePath, DocumentDirectoryPath, TemporaryDirectoryPath, readFile, readDir, stat, copyFile, unlink } from 'react-native-fs'
 
  type SharedItem = {
    mimeType: string,
@@ -49,17 +49,21 @@
    if (sharedMimeType.startsWith('application/zip')){
      const sourcePath = sharedData;
      console.log(sourcePath);
-     const tempPath = `${TemporaryDirectoryPath}/tester.zip`;
+     var n = Math.random();
+     const tempPath = `${TemporaryDirectoryPath}/tester${n}.zip`;
      copyFile(sourcePath, tempPath)
      .catch((error) => {
        console.error(error)
      });
-     console.log(tempPath);
+     console.log("tempPath:", tempPath);
      const targetPath = DocumentDirectoryPath
      const charset = 'UTF-8'
      unzip(tempPath, targetPath, charset)
       .then((path) => {
         console.log(`unzip completed at ${path}`)
+
+        unlink(tempPath);
+
         readDir(DocumentDirectoryPath)
         .then((result) => {
           console.log('test readdir', result);
@@ -67,7 +71,7 @@
           while(result[i]["name"]!="_chat.txt"){
             i = i+1;
           }
-          console.log(result[i]["path"]);
+          console.log("result:" , result[i]["path"]);
           return readFile(result[i]["path"]);
         })
         .then((contents) => {
@@ -132,112 +136,3 @@
  });
 
  export default App;
-
-/*
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
-
-export default App;
-*/
